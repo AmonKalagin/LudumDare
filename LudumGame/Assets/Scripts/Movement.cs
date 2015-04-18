c989dfdfec2571b005c550a47aa28c59b2a84972
AmonKalagin <amon.kalagin@gmail.com>
2015-04-18 15:03:06 +0200

    left and right turning

--- LudumGame/Assets/Scripts/Movement.cs
+++ LudumGame/Assets/Scripts/Movement.cs
@@ -3,17 +3,48 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 	public float moveSpeed;
+	private float maxSpeed = 7f;
 
 	private Vector3 input;
 
-
 	
-	void Start () {
+	void Start () 
+	{
 	
 	}
 
-	void Update () {
-		input = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-		GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+	void Update () 
+	{
+		input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		if (transform.localRotation.y < 0.7) 
+		{
+			if (Input.GetAxisRaw ("Horizontal") > 0) 
+			{ 
+				transform.Rotate (Vector3.up, 100 * Time.deltaTime);
+				Debug.Log ("y" + transform.localRotation.y);
+				Debug.Log ("x" + transform.localRotation.x);
+				Debug.Log ("z" + transform.localRotation.z);
+
+			}
+		}
+		if (transform.localRotation.y > -0.7) 
+		{
+			if (Input.GetAxisRaw ("Horizontal") < 0) 
+			{ 
+				transform.Rotate (Vector3.down, 100 * Time.deltaTime);
+				Debug.Log ("y" + transform.localRotation.y);
+				Debug.Log ("x" + transform.localRotation.x);
+				Debug.Log ("z" + transform.localRotation.z);
+				
+			}
+		}
+			
+		if(GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+		{
+
+			GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+		}
+
+
 	}
 }
---
+++ LudumGame/Assets/Scripts/ProjectileShoot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileShoot : MonoBehaviour {
+
+	GameObject prefab;
+	void Start () {
+		prefab = Resources.Load ("projectile") as GameObject;
+	}
+
+	void Update () {
+	if (Input.GetKeyDown ("space")) {
+			GameObject projectile = Instantiate(prefab) as GameObject;
+			projectile.transform.position = transform.position *2;
+			Rigidbody rb = projectile.GetComponent<Rigidbody>();
+			rb.velocity = Camera.main.transform.forward*1;
+		}
+	}
+}
