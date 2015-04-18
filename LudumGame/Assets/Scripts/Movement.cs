672bf2116138f46193a99e453859c83a12ad0948
AmonKalagin <amon.kalagin@gmail.com>
2015-04-18 12:44:24 +0200

    basic movement

---
+++ LudumGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed;
+
+	private Vector3 input;
+
+
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		input = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+	}
+}
